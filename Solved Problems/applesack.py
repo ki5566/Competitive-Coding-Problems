3dbb239a6975b97100354ef2bf2019b873ef010e
ki5566 <106714647+ki5566@users.noreply.github.com>
2023-03-06 23:37:08 -0600

    Update applesack.py

--- Solved Problems/applesack.py
+++ Solved Problems/applesack.py
@@ -1,3 +1,16 @@
+# BEGIN ANNOTATION
+# PROBLEM URL: https://open.kattis.com/problems/applesack
+# TAGS: Greedy, Resource efficiency
+# EXPLANATION: We can consider the base case where the number of apples left at some position is k,
+# the maximum amount the sack can hold. From this case it is obvious the best option is to move forward k+1 steps,
+# using all the apples. Backtracking we can see we want to bring as many apples as possible as far as possible
+# before we are left with k apples. We want to be as efficient as possible with moving apples forward.
+# This leads to 2 conclusions. First, apples will never travel backwards.
+# Second, with each apple consumed to move forward one, we must bring forward as many apples as possible
+# This means k-1 apples will pass through for every 1 apple consumed, which is as optimal as possible.
+# Repeat until there are equal or less than k apples.
+# END ANNOTATION
+
 import math
 
 [n,k] = [int(i) for i in input().split()]
